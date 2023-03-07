f7c536af3868845958ab71818239995cd65f479b
Marion <mrobert148@gmail.com>
2023-03-07 10:51:06 +0100

    flipping cards

--- main.py
+++ main.py
@@ -7,23 +7,36 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 # ______ CREATE NEW FLASH CARDS _____
 data = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
+current_card = {}
+
 
 def create_flashcard():
-   word = random.choice(data)
-   canvas.itemconfig(card_title, text="French")
-   canvas.itemconfig(card_word, text=word["French"])
+   global current_card, flip_timer
+   window.after_cancel(flip_timer)
+   current_card = random.choice(data)
+   canvas.itemconfig(bg_image, image=card_front_photo)
+   canvas.itemconfig(card_title, text="French", fill="black")
+   canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+   flip_timer = window.after(3000, func=show_answer)
 
+
+def show_answer():
+   canvas.itemconfig(bg_image, image=card_back_photo)
+   canvas.itemconfig(card_title, text="English", fill="white")
+   canvas.itemconfig(card_word, text=current_card["English"], fill="white")
 
 
 # ______ SET TUP THE WINDOW _____
 window = Tk()
 window.title("Flash Card App")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=show_answer)
 
 # ______ CARD AS A CANVAS _____
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_photo = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_photo)
+card_back_photo = PhotoImage(file="images/card_back.png")
+bg_image = canvas.create_image(400, 263, image=card_front_photo)
 card_title = canvas.create_text(400, 150, text="",font=("Arial", 40, "italic"))
 card_word = canvas.create_text(400, 300, text="", font=("Arial", 56, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
